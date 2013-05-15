8f2eef4d47e3dd20799ce16bd116e16340aa06a1
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-15 16:11:45 -0400

    feat(setup): make pip installable.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+pip_requires = os.path.join(os.getcwd(), 'tools', 'pip-requires')
+test_requires = os.path.join(os.getcwd(), 'tools', 'test-requires')
+
+def file_lines(path):
+    reqs = None
+    with open(path, 'rt') as f:
+        reqs = f.read().split()
+    return reqs
+
+setup(
+    name='rackspace-backup-client',
+    version='0.0.2',
+    author='Alejandro Cabrera',
+    author_email='alejandro.cabrera@rackspace.com',
+    description='A Python client for the Rackspace Cloud Backup API.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/cabrera/rackspace-backup-client',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=file_lines(pip_requires),
+    include_package_data=True,
+    classifiers=file_lines('docs/CLASSIFIERS')
+)
