a25dadc743485f095fdfb29ef8ecc5fed165b5d4
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-15 17:32:03 -0400

    feat(auth): add authentication + tests.

---
+++ rcbu/common/auth.py
@@ -0,0 +1,38 @@
+import json
+
+import requests
+
+from rcbu.common.constants import IDENTITY_TOKEN_URL
+
+def authenticate(apikey=None, password=None, *, username):
+    if apikey:
+        return _auth(username=username, apikey=apikey)
+
+    if password:
+        return _auth(username=username, password=password)
+
+
+def get_token(apikey=None, password=None, *, username):
+    r = authenticate(username=username, apikey=apikey, password=password)
+    return r.json()['access']['token']['id']
+
+
+def _auth(apikey=None, password=None, *, username):
+    creds = {'username': username}
+    assert password or apikey
+
+    auth_type = None
+    if password:
+        creds['password'] = password
+        auth_type = {'passwordCredentials': creds}
+    else:
+        creds['apiKey'] = apikey
+        auth_type = {'RAX-KSKEY:apiKeyCredentials': creds}
+        
+    wrapper = {'auth': auth_type}
+    data = json.dumps(wrapper)
+    r = requests.post(IDENTITY_TOKEN_URL, data=data,
+                      headers={'content-type': 'application/json'})
+    if not r.ok:
+        r.raise_for_status()
+    return r
---
+++ tests/unit/common/test_auth.py
@@ -0,0 +1,49 @@
+import unittest
+
+from httpretty import HTTPretty, httprettified
+
+import rcbu.common.auth as auth
+
+
+def _prepare_mock_post(status, body=None):
+    HTTPretty.register_uri(HTTPretty.POST, '_',
+                           status=status, body=body)
+    
+
+class TestAuth(unittest.TestCase):
+
+    @httprettified
+    def test_auth_by_password_raises_if_invalid_creds(self):
+        _prepare_mock_post(status=403)
+        self.assertRaises(Exception,
+                          auth.authenticate(password='bad', username='b'))
+
+    @httprettified
+    def test_auth_by_apikey_raises_if_invalid_creds(self):
+        _prepare_mock_post(status=403)
+        self.assertRaises(Exception,
+                          auth.authenticate(apikey='bad', username='b'))
+
+    @httprettified
+    def test_auth_returns_catalog_on_success(self):
+        _prepare_mock_post(status=200, body='yay')
+        catalog = auth.authenticate(username='b', password='good')
+        self.assertEqual(catalog, 'yay')
+
+    @httprettified
+    def test_get_token_by_password_raises_if_invalid_creds(self):
+        _prepare_mock_post(status=403)
+        self.assertRaises(Exception,
+                          auth.get_token(password='bad', username='b'))
+
+    @httprettified
+    def test_get_token_by_apikey_raises_if_invalid_creds(self):
+        _prepare_mock_post(status=403)
+        self.assertRaises(Exception,
+                          auth.get_token(apikey='bad', username='b'))
+
+    @httprettified
+    def test_get_token_returns_token_on_success(self):
+        _prepare_mock_post(status=200, body='token')
+        token = auth.get_token(username='b', password='good')
+        self.assertEqual(token, 'token')
