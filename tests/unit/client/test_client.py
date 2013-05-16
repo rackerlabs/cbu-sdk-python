954abc967b855675b7649a7c70ea2255c2be0049
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-16 14:20:03 -0400

    feat(tests): mock the whole thing.

--- tests/unit/client/test_client.py
+++ tests/unit/client/test_client.py
@@ -1,47 +1,82 @@
+import json
 import unittest
 
 from httpretty import HTTPretty, httprettified
 from requests.exceptions import HTTPError
 
 from rcbu.client.client import Connection
+from rcbu.common.constants import IDENTITY_TOKEN_URL
+
+
+MOCK_KEY = 'key'
+MOCK_ENDPOINT = 'http://tacobackup.com/v1.0/912371'
+MOCK_ENDPOINT_STRIPPED = 'http://tacobackup.com/v1.0'
+
+
+def _mock_auth(status):
+    backup_endpoint = [
+        {
+            'type': 'rax:backup',
+            'endpoints': [
+                {
+                    'publicURL': MOCK_ENDPOINT
+                }
+            ]
+        },
+        {
+            'type': 'something:else',
+            'endpoints': [
+                {
+                    'publicURL': MOCK_ENDPOINT + '/not-right'
+                }
+            ]
+        }
+    ]
+
+    reply = {
+        'access': {
+            'token': {
+                'id': MOCK_KEY
+            },
+            'serviceCatalog': backup_endpoint
+        }
+    }
+
+    HTTPretty.register_uri(HTTPretty.POST, IDENTITY_TOKEN_URL,
+                           status=status, body=json.dumps(reply))
 
 
 class TestValidConnection(unittest.TestCase):
+
+    @httprettified
     def setUp(self):
-        self.conn = Connection('cppcabrera', password='$x4dc4dv4d$')
+        _mock_auth(200)
+        self.conn = Connection('a', password='a')
 
     def test_connection_has_correct_properties(self):
-        self.assertEqual(self.conn.token,
-                         '2560cd91-bdf5-4e6d-9ae8-a33444e84b22')
-        self.assertEqual(self.conn.endpoint,
-                         'https://backup.api.rackspacecloud.com/v1.0')
+        self.assertEqual(self.conn.token, MOCK_KEY)
+        self.assertEqual(self.conn.endpoint, MOCK_ENDPOINT_STRIPPED)
 
-    def test_agents_acquired(self):
-        agents = self.conn.agents
-        self.assertIsNotNone(agents)
-        self.assertIsNot(agents, [])
+    @httprettified
+    def test_agents_raises_403_on_invalid_auth(self):
+        url = self.conn.endpoint + '/user/agents'
+        HTTPretty.register_uri(HTTPretty.GET, url, status=403)
+        with self.assertRaises(HTTPError):
+            self.conn.agents
 
-    def test_backup_configurations_acquired(self):
-        configs = self.conn.backup_configurations
-        self.assertIsNotNone(configs)
-        self.assertIsNot(configs, [])
+    @httprettified
+    def test_backup_configurations_raises_403_on_invalid_auth(self):
+        url = self.conn.endpoint + '/backup-configuration'
+        HTTPretty.register_uri(HTTPretty.GET, url, status=403)
+        with self.assertRaises(HTTPError):
+            self.conn.backup_configurations
 
     def test_host(self):
-        self.assertEqual(self.conn.host,
-                         'https://backup.api.rackspacecloud.com/v1.0')
+        self.assertEqual(self.conn.host, MOCK_ENDPOINT_STRIPPED)
+
 
     def test_version(self):
         self.assertEqual(self.conn.api_version, '1.0')
 
     def test_version_tuple(self):
         self.assertEqual(self.conn.api_version_tuple, (1, 0))
-
-    def test_active_backups(self):
-        backups = self.conn.active_backups
-        self.assertIsNotNone(backups)
-        self.assertIsNot(backups, [])
-
-    def test_active_restores(self):
-        restores = self.conn.active_restores
-        self.assertIsNotNone(restores)
-        self.assertIsNot(restores, [])
