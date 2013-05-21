d29faa04e48fe6a52ae1bbe8541e166687b617bf
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-21 16:36:26 -0400

    feat(backup_report): add basic functionality for backup report.

--- rcbu/client/backup_report.py
+++ rcbu/client/backup_report.py
@@ -1,18 +1,67 @@
 from rcbu.client.report import Report
 
 
+def _args_from_dict(body):
+    args = {
+        '_config_id': body['BackupConfigurationId'],
+        '_config_name': body['BackupConfigurationName'],
+        '_agent_id': body['MachineAgentId'],
+        '_machine_name': body['ComputerName'],
+        '_state': body['State'],
+        '_restorable': body['CanRestore'],
+        '_time': {
+            'start': body['StartTime'],
+            'end': body['CompletedTime'],
+            'duration': body['Duration']
+        },
+        '_searched': {
+            'files': body['FilesSearched'],
+            'bytes': body['BytesSearched']
+        },
+        '_backup': {
+            'files': body['FilesBackedUp'],
+            'bytes': body['BytesBackedUp']
+        },
+        '_num_errors': body['NumErrors'],
+        '_outcome': body['Reason'],
+        '_diagnostics': body['Diagnostics'],
+        '_errors': body['ErrorList']
+    }
+    return args
+
+
+def from_dict(body, connection=None):
+    args = _args_from_dict(body)
+    return BackupReport(body['BackupId'], connection, **args)
+
+
+class BackupFailed(Exception):
+    def __init__(self, report):
+        self.message = '{0}:{1}'.format(report._diagnostics, report.errors)
+
+
 class BackupReport(Report):
-    def __init__(self, report_id):
-        pass
+    def __init__(self, backup_id, connection=None, **kwargs):
+        self.backup_id = backup_id
+        self._connection = connection
+        [setattr(self, k, v) for k, v in kwargs.items()]
 
-    def get_configuration(self):
-        pass
+    @property
+    def id(self):
+        return self.backup_id
 
-    def files_searched(self):
-        pass
+    @property
+    def errors(self):
+        return self._errors
 
-    def bytes_searched(self):
-        pass
+    @property
+    def outcome(self):
+        return self._outcome
 
-    def bytes_backed_up(self):
-        pass
+    @property
+    def ok(self):
+        return self._restorable
+
+    def raise_if_not_ok(self):
+        if not self.ok:
+            raise BackupFailed(self)
