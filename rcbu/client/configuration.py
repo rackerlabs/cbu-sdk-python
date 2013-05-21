81998f287e5809b5706e27e459a88fe2ffb98fce
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-21 13:00:08 -0400

    refactor: clean up configuration namespace.

--- rcbu/client/configuration.py
+++ rcbu/client/configuration.py
@@ -9,40 +9,5 @@ class Configuration(Show):
         return '{}:{}'.format('Configuration', self.config_id)
 
     @property
-    def schedule(self):
-        pass
-
-    @property
     def id(self):
         return self.config_id
-
-    @property
-    def notification_email(self):
-        pass
-
-    def next_runtime(self):
-        pass
-
-    def is_enabled(self):
-        pass
-
-    def notifies_on_failure(self):
-        pass
-
-    def notifies_on_success(self):
-        pass
-
-    def update_from_file(self, path):
-        pass
-
-    def update_from_dict(self, conf):
-        pass
-
-    def disable(self):
-        pass
-
-    def enable(self):
-        pass
-
-    def delete(self):
-        pass
