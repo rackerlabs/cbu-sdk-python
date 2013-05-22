6d29d1ec50aad412c7d3b1ebed1dd01487765cd0
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-22 09:18:11 -0400

    fix(version): rm dependency on README.

--- rcbu/version.py
+++ rcbu/version.py
@@ -1,13 +1,2 @@
-import re
-
-
-def parse_version():
-    data = None
-    with open('../README.rst', 'rt') as f:
-        data = f.read()
-    version_re = r':version: (\d)\.(\d)\.(\d)'
-    return tuple([int(k) for k in re.search(version_re, data).groups()])
-
-
-version_tuple = parse_version()
-version = '.'.join(version_tuple)
+version_tuple = (0, 6, 0)
+version = '.'.join([str(k) for k in version_tuple])
