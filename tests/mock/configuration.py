f267c09aea793e4df2ea0869696eaec57576a88c
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-25 14:09:11 -0400

    tests(backup_config): add several unit tests.

--- tests/mock/configuration.py
+++ tests/mock/configuration.py
@@ -1,2 +1,23 @@
-def backup_configuration():
-    return None
+def backup_configuration(agent_id=1, is_active=True,
+                         is_deleted=False, is_encrypted=False):
+    return {
+        'MachineAgentId': agent_id,
+        'BackupConfigurationName': 'mock',
+        'IsActive': is_active,
+        'IsDeleted': is_deleted,
+        'IsEncrypted': is_encrypted,
+        'VersionRetention': 0,
+        'MissedBackupActionId': 1,
+        'Frequency': 'Manually',
+        'StartTimeHour': None,
+        'StartTimeMinute': None,
+        'StartTimeAmPm': None,
+        'DayOfWeekId': None,
+        'HourInterval': None,
+        'TimeZoneId': 'Eastern Standard Time',
+        'NotifyRecipients': 'mock@mock.com',
+        'NotifySuccess': False,
+        'NotifyFailure': True,
+        'Inclusions': [],
+        'Exclusions': []
+    }
