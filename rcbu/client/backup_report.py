0654106e841a090ea1546142c00609a56207bac0
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-31 21:58:52 -0400

    refactor/feat(backup_report): rm common fields, delegate to parent, +funcs.

--- rcbu/client/backup_report.py
+++ rcbu/client/backup_report.py
@@ -1,20 +1,12 @@
-from rcbu.client.report import Report
+import rcbu.client.report as report
 from rcbu.common.exceptions import BackupFailed
 
 
 def _args_from_dict(body):
     args = {
-        '_config_id': body['BackupConfigurationId'],
-        '_config_name': body['BackupConfigurationName'],
         '_agent_id': body['MachineAgentId'],
         '_machine_name': body['ComputerName'],
-        '_state': body['State'],
         '_restorable': body['CanRestore'],
-        '_time': {
-            'start': body['StartTime'],
-            'end': body['CompletedTime'],
-            'duration': body['Duration']
-        },
         '_searched': {
             'files': body['FilesSearched'],
             'bytes': body['BytesSearched']
@@ -22,42 +14,37 @@ def _args_from_dict(body):
         '_backup': {
             'files': body['FilesBackedUp'],
             'bytes': body['BytesBackedUp']
-        },
-        '_num_errors': body['NumErrors'],
-        '_outcome': body['Reason'],
-        '_diagnostics': body['Diagnostics'],
-        '_errors': body['ErrorList']
+        }
     }
+    args.update(report._args_from_dict(body))
     return args
 
 
-def from_dict(body, connection=None):
+def from_dict(backup_id, body):
     args = _args_from_dict(body)
-    return BackupReport(body['BackupId'], connection, **args)
+    return BackupReport(backup_id, **args)
 
 
 class BackupReport(Report):
-    def __init__(self, backup_id, connection=None, **kwargs):
-        self.backup_id = backup_id
-        self._connection = connection
-        [setattr(self, k, v) for k, v in kwargs.items()]
-
-    @property
-    def id(self):
-        return self.backup_id
-
-    @property
-    def errors(self):
-        return self._errors
-
-    @property
-    def outcome(self):
-        return self._outcome
+    def __init__(self, report_id, **kwargs):
+        Report.__init__(self, report_id, kwargs)
 
     @property
     def ok(self):
         return self._restorable
 
-    def raise_if_not_ok(self):
-        if not self.ok:
-            raise BackupFailed(self)
+    @property
+    def files_searched(self):
+        return self._searched['files']
+
+    @property
+    def bytes_searched(self):
+        return self._searched['bytes']
+
+    @property
+    def files_saved(self):
+        return self._backup['files']
+
+    @property
+    def bytes_saved(self):
+        return self._backup['bytes']
