f51413bd205808a41dfdf9e08e8393e5a6ec48c0
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-05-31 21:58:00 -0400

    refactor/feat(restore_report): rm common fields, delegate to parent, +funcs.

--- rcbu/client/restore_report.py
+++ rcbu/client/restore_report.py
@@ -1,18 +1,10 @@
-from rcbu.client.report import Report
+import rcbu.client.report as report
 from rcbu.common.exceptions import RestoreFailed
 
 
 def _args_from_dict(body):
-    return {
-        '_config_id': body['BackupConfigurationId'],
-        '_config_name': body['BackupConfigurationName'],
+    args = {
         '_backup_id': body['BackupConfigurationId'],
-        '_state': body['State'],
-        '_time': {
-            'start': body['StartTime'],
-            'end': body['CompletedTime'],
-            'duration': body['Duration']
-        },
         '_restored': {
             'files': body['NumFilesRestored'],
             'bytes': body['NumBytesRestored']
@@ -20,12 +12,10 @@ def _args_from_dict(body):
         '_destination': {
             'id': body['RestoreDestinationMachineId'],
             'name': body['RestoreDestination']
-        },
-        '_num_errors': body['NumErrors'],
-        '_outcome': body['Reason'],
-        '_diagnostics': body['Diagnostics'],
-        '_errors': body['ErrorList']
+        }
     }
+    args.update(report._args_from_dict(body))
+    return args
 
 
 def from_dict(restore_id, body):
@@ -34,26 +24,13 @@ def from_dict(restore_id, body):
 
 
 class RestoreReport(Report):
-    def __init__(self, restore_id, **kwargs):
-        self._id = restore_id
-        [setattr(self, k, v) for k, v in kwargs.items()]
+    def __init__(self, report_id, **kwargs):
+        Report.__init__(self, report_id, kwargs)
 
     @property
-    def id(self):
-        return self._id
+    def files_restored(self):
+        return self._restored['files']
 
     @property
-    def errors(self):
-        return self._errors
-
-    @property
-    def outcome(self):
-        return self._outcome
-
-    @property
-    def ok(self):
-        return len(self.errors) == 0
-
-    def raise_if_not_ok(self):
-        if not self.ok:
-            raise RestoreFailed(self)
+    def bytes_restored(self):
+        return self._restored['bytes']
