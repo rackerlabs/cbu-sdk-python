0f728f82a47b921b6d1d23341a3aab9706345a73
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-06-03 17:47:29 -0400

    fix/refactor(exceptions): fix error in exception raising, improve structure.

--- rcbu/common/exceptions.py
+++ rcbu/common/exceptions.py
@@ -1,14 +1,20 @@
-class BackupFailed(Exception):
+class OperationFailed(RuntimeError):
+    def __init__(self, op, report):
+        message = '{0}: {1}:{2}'.format(op, report._diagnostics, report.errors)
+        RuntimeError.__init__(self, message)
+
+
+class BackupFailed(OperationFailed):
     def __init__(self, report):
-        self.message = '{0}:{1}'.format(report._diagnostics, report.errors)
+        OperationFailed.__init__(self, 'Backup', report)
 
 
 class RestoreFailed(Exception):
     def __init__(self, report):
-        self.message = '{0}:{1}'.format(report._diagnostics, report.errors)
+        OperationFailed.__init__(self, 'Restore', report)
 
 
-class InconsistentInclusionsError(Exception):
+class InconsistentInclusionsError(ValueError):
     def __init__(self, diff_set):
         msg_template = '{0} are included and excluded. Which did you mean?'
-        self.message = msg_template.format(diff_set)
+        ValueError.__init__(msg_template.format(diff_set))
