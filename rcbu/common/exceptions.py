2f6d6628726a09714321d3187ec9c4c7b9e24c32
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-06-03 17:37:21 -0400

    feat(exception): add InconsistentInclusionError.

--- rcbu/common/exceptions.py
+++ rcbu/common/exceptions.py
@@ -6,3 +6,9 @@ class BackupFailed(Exception):
 class RestoreFailed(Exception):
     def __init__(self, report):
         self.message = '{0}:{1}'.format(report._diagnostics, report.errors)
+
+
+class InconsistentInclusionsError(Exception):
+    def __init__(self, diff_set):
+        msg_template = '{0} are included and excluded. Which did you mean?'
+        self.message = msg_template.format(diff_set)
