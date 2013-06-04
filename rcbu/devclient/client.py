46519bef51438908729dde184f1b681ec989c881
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-06-03 23:06:07 -0400

    refactor(dev): dev interface should not live here.

--- rcbu/devclient/client.py
+++
@@ -1,9 +0,0 @@
-from rcbu.client.client import Connection as UserConnection
-
-
-class Connection(UserConnection):
-    def __init__(self, username, apikey, datacenter="dfw1"):
-        pass
-
-    def register_agent(self):
-        pass
