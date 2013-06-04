e020cd2a7e1afd2f26c36b8c0a6da90eaa755014
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-06-04 09:44:02 -0400

    refactor(client): update to use new connection request method.

--- rcbu/client/client.py
+++ rcbu/client/client.py
@@ -19,53 +19,41 @@ class Client(Show, ExposesActivities):
     @property
     def agents(self):
         url = self.endpoint + '/user/agents'
-        headers = {'x-auth-token': self.token}
-        resp = requests.get(url, headers=headers, verify=False)
+        resp = self._connection.request(requests.get(url))
         resp.raise_for_status()
-        body = resp.json()
-        return [agent.from_dict(a, connection=self) for a in body]
+        return [agent.from_dict(a, connection=self._connection) for a in body]
 
     @property
     def backup_configurations(self):
         url = self.endpoint + '/backup-configuration'
-        headers = {'x-auth-token': self.token}
-        resp = requests.get(url, headers=headers, verify=False)
-        resp.raise_for_status()
+        resp = self._connection.request(requests.get, url)
         body = resp.json()
         return [backup_config.from_dict(config, self) for config in body]
 
     def get_agent(self, agent_id):
         url = '{0}/{1}/{2}'.format(self.endpoint, 'agent', agent_id)
-        headers = {'x-auth-token': self.token}
-        resp = requests.get(url, headers=headers, verify=False)
-        resp.raise_for_status()
-        return agent.from_dict(resp.json(), connection=self)
+        resp = self._connection.request(requests.get, url)
+        return agent.from_dict(resp.json(), connection=self._connection)
 
     def get_backup_configuration(self, config_id):
         url = '{0}/{1}/{2}'.format(self.endpoint, 'backup-configuration',
                                    config_id)
-        headers = {'x-auth-token': self.token}
-        resp = requests.get(url, headers=headers, verify=False)
-        resp.raise_for_status()
+        resp = self._connection.request(requests.get, url)
         return backup_config.from_dict(resp.json())
 
     def get_backup_report(self, backup_id):
         url = '{0}/{1}/{2}/{3}'.format(self.endpoint, 'backup', 'report',
                                        backup_id)
-        headers = {'x-auth-token': self.token}
-        resp = requests.get(url, headers=headers, verify=False)
-        resp.raise_for_status()
+        resp = self._connection.request(requests.get, url)
         return resp.json()
 
     def create_backup(self, config):
-        backup_action = backup.Backup(config, connection=self)
+        backup_action = backup.Backup(config, connection=self._connection)
         return backup_action
 
     def create_restore(self, backup_id, source_agent, destination_path,
                        destination_agent=None, overwrite=False):
         url = '{0}/{1}'.format(self.endpoint, 'restore')
-        headers = {'x-auth-token': self.token,
-                   'content-type': 'application/json'}
         data = json.dumps({
             'BackupId': backup_id,
             'BackupMachineId': source_agent.id,
@@ -74,7 +62,7 @@ class Client(Show, ExposesActivities):
             'DestinationPath': destination_path,
             'OverwriteFiles': overwrite
         })
-        resp = requests.put(url, headers=headers, data=data, verify=False)
-        resp.raise_for_status()
-        restore_action = restore.from_dict(resp.json(), connection=self)
+        resp = self._connection.request(requests.put, url, data=data)
+        restore_action = restore.from_dict(resp.json(),
+                                           connection=self._connection)
         return restore_action
