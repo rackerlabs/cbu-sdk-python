e5b2044ea01cdc6d727225e1bc7e41bee124cb58
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-06-06 16:07:43 -0400

    feat(weekdays): add to string.

--- rcbu/common/weekdays.py
+++ rcbu/common/weekdays.py
@@ -11,3 +11,17 @@ class Weekdays(object):
     Thursday = 4
     Friday = 5
     Saturday = 6
+
+    _to_string = {
+        0: 'Sunday',
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday'
+    }
+
+    @classmethod
+    def str(cls, value):
+        return cls._to_string[value]
