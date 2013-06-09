3618baae1649cdb0e752dbe0c80462e311f70bb1
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-06-09 10:18:48 -0400

    tests(backup_config): add several new tests.

--- tests/unit/client/test_backup_configuration.py
+++ tests/unit/client/test_backup_configuration.py
@@ -7,7 +7,10 @@ from rcbu.client.connection import Connection
 from rcbu.common.constants import IDENTITY_TOKEN_URL
 import tests.mock.auth as mock_auth
 import tests.mock.configuration as mock_config
-import rcbu.client.backup_configuration as bconf
+import rcbu.client.backup_configuration as backup_config
+from rcbu.common.exceptions import (
+    DisconnectedError, InconsistentInclusionsError
+)
 
 
 def _mock_auth(status):
@@ -23,24 +26,19 @@ def _mock_config(status, endpoint, config_id):
 
 
 def _quick_config(connection=None):
-    return bconf.from_dict(mock_config.backup_configuration(),
-                           connection=connection)
+    return backup_config.from_dict(mock_config.backup_configuration(),
+                                   connection=connection)
 
 
-class TestBackupConfiguration(unittest.TestCase):
-    @httprettified
-    def setUp(self):
-        _mock_auth(200)
-        self.connection = Connection(username='a', password='a')
-
+class TestLoadBackupConfiguration(unittest.TestCase):
     def test_from_dict_works_as_expected(self):
         body = mock_config.backup_configuration()
-        config = bconf.from_dict(body)
+        config = backup_config.from_dict(body)
         self.assertEqual(config.id, 0)
         self.assertEqual(config.agent_id, 1)
-        self.assertEqual(config.notification_settings['on_failure'], True)
-        self.assertEqual(config.notification_settings['on_success'], False)
-        self.assertEqual(config.notification_settings['email'],
+        self.assertEqual(config.notify_on_failure, True)
+        self.assertEqual(config.notify_on_success, False)
+        self.assertEqual(config.email,
                          'mock@mock.com')
         self.assertEqual(config.name, 'mock')
         self.assertEqual(config.encrypted, False)
@@ -53,61 +51,81 @@ class TestBackupConfiguration(unittest.TestCase):
             mock = mock_config.backup_configuration()
             del mock[key]
             with self.assertRaises(KeyError):
-                bconf.from_dict(mock)
+                backup_config.from_dict(mock)
 
-    def test_to_json_contains_expected_fields(self):
-        mock = mock_config.backup_configuration()
-        config = bconf.from_dict(mock)
-        parsed = json.loads(bconf.to_json(config))
 
-        # These keys aren't needed
-        del mock['IsDeleted']
-        del mock['IsEncrypted']
+class TestBackupConfiguration(unittest.TestCase):
+    @httprettified
+    def setUp(self):
+        _mock_auth(200)
+        self.connection = Connection(username='a', password='a')
+        config_api = mock_config.backup_configuration()
+        self.config = backup_config.from_dict(config_api, self.connection)
 
-        for key in mock.keys():
-            self.assertIn(key, parsed)
+    def test_id_matches_expected(self):
+        self.assertEqual(self.config.id, 0)
+
+    def test_agent_id_matches_expected(self):
+        self.assertEqual(self.config.agent_id, 1)
+
+    def test_name_matches_expected(self):
+        self.assertEqual(self.config.name, 'mock')
+
+    def test_notification_settings_match_expected(self):
+        self.assertEqual(self.config.email, 'mock@mock.com')
+        self.assertEqual(self.config.notify_on_success, False)
+        self.assertEqual(self.config.notify_on_failure, True)
+
+    def test_enabled_matches_expected(self):
+        self.assertEqual(self.config.enabled, True)
+
+    def test_encrypted_matches_expected(self):
+        self.assertEqual(self.config.encrypted, False)
+
+    def test_deleted_matches_expected(self):
+        self.assertEqual(self.config.deleted, False)
 
     def test_update_notification_settings_works(self):
-        config = _quick_config()
-        config.update_notification_settings('woot',
-                                            notify_on_failure=False,
-                                            notify_on_success=True)
-        settings = config.notification_settings
-        self.assertEqual(settings['email'], 'woot')
-        self.assertEqual(settings['on_success'], True)
-        self.assertEqual(settings['on_failure'], False)
+        self.config.update_notification_settings(email='woot',
+                                                 notify_on_failure=False,
+                                                 notify_on_success=True)
+        self.assertEqual(self.config.email, 'woot')
+        self.assertEqual(self.config.notify_on_success, True)
+        self.assertEqual(self.config.notify_on_failure, False)
 
-    def test_change_name_works(self):
-        config = _quick_config()
-        config.change_name('woot')
-        self.assertEqual(config.name, 'woot')
+    def test_rename_works(self):
+        self.config.rename('woot')
+        self.assertEqual(self.config.name, 'woot')
 
     def test_disconneted_error_raised_if_no_connection(self):
-        config = _quick_config()
-        with self.assertRaises(bconf.DisconnectedError):
-            config.enable()
-
-    def test_connect_works(self):
-        config = _quick_config()
-        config.connect(self.connection)
-        self.assertIs(config._connection, self.connection)
+        self.config.connect(None)
+        with self.assertRaises(DisconnectedError):
+            self.config.enable()
 
     @httprettified
     def _test_toggle(self, enabled=True):
-        config = _quick_config(self.connection)
         url = '{0}/{1}/{2}/{3}'.format(self.connection.host,
                                        'backup-configuration', 'enable',
-                                       config.id)
+                                       self.config.id)
         HTTPretty.register_uri(HTTPretty.POST, url, status=200,
                                body=json.dumps({'IsActive': enabled}))
         if enabled:
-            config.enable()
+            self.config.enable()
         else:
-            config.disable()
-        self.assertEqual(config.enabled, enabled)
+            self.config.disable()
+        self.assertEqual(self.config.enabled, enabled)
 
     def test_disable_works(self):
         self._test_toggle(enabled=False)
 
     def test_enable_works(self):
         self._test_toggle(enabled=True)
+
+    @httprettified
+    def test_delete_works(self):
+        url = '{0}/backup-configuration/{1}'.format(self.connection.host,
+                                                    self.config.id)
+        HTTPretty.register_uri(HTTPretty.DELETE, url, status=204)
+        self.assertEqual(self.config.deleted, False)
+        self.config.delete()
+        self.assertEqual(self.config.deleted, True)
