e1bfa18d2b5e07fc6d31431bc152235669cb4b62
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-06-24 17:37:38 -0400

    style(utils/bytes): fix flake8 violations.

--- rcbu/utils/bytes.py
+++ rcbu/utils/bytes.py
@@ -1,11 +1,12 @@
 """Utilities for handling byte quantities and strings."""
 
+
 def dehumanize_bytes(human_bytes):
     """Convert a string in the format '2.23 GB' -> 2.23 * 10**30"""
     packed = human_bytes.split()
     amount, magnitude = packed[0], None
     if len(packed) == 2:
-      magnitude = packed[1].upper()
+        magnitude = packed[1].upper()
     prefixes = ['YB', 'ZB', 'EB', 'PB', 'TB', 'GB', 'MB', 'KB']
     multipliers = [2**(i*10) for i in range(8, 0, -1)]
     magnitude_to_multiplier = {ma: mult for ma, mult in
